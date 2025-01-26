63af930a8d063a6505ed0d94624c3a0272b61fa3
mi-wa-kuro <gyif1914@yahoo.co.jp>
2025-01-26 22:24:27 +0900

    javascript4宿題

--- javascript-lesson/work/javascript-lesson-3/lesson-3-5.js
+++ javascript-lesson/work/javascript-lesson-3/lesson-3-5.js
@@ -1,29 +1,29 @@
 let numbers = [7, 12, 5, 9, 3, 15];
-let rengthMinus = 0
+let rengthMinus = 0;
 if(numbers[0] % 2 != 0){
-  rengthMinus == 1;
+  rengthMinus += 1;
   
 }
 if(numbers[1] % 2 != 0){
-  rengthMinus == 1;
+  rengthMinus += 1;
   
 }
 if(numbers[2] % 2 != 0){
-  rengthMinus == 1;
+  rengthMinus += 1;
   
 }
 if(numbers[3] % 2 != 0){
-  rengthMinus == 1;
+  rengthMinus += 1;
   
 }
 if(numbers[4] % 2 != 0){
-  rengthMinus == 1;
+  rengthMinus += 1;
   
 }
 if(numbers[5] % 2 != 0){
-  rengthMinus == 1;
+  rengthMinus += 1;
   
 }
-  console.log(numbers. length + rengthMinus);
+  console.log(rengthMinus);
 
  
